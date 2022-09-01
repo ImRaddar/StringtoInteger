23220b1644092322d4a94c0bdcd3a9004f814b6f
Royal <51541989+ImRaddar@users.noreply.github.com>
2022-09-01 23:30:08 +0100

    Main file
    hi

---
+++ main.py
@@ -0,0 +1,40 @@
+import re
+
+def myAtoi(s: str):
+    pattern = r"[+-]*\d+"
+    if len(s) == 0:
+        return 0
+    if len(re.findall(pattern, s)) == 0:
+        return 0
+    num = re.findall(pattern, s)[0]
+    resultsign = num.find('+')
+    if resultsign == -1: 
+        resultsign = num.find('-')
+    if resultsign != -1:
+        if resultsign != 0:
+            return 0
+        if num[resultsign+1] == '-' or num[resultsign+1] == '+':
+            return 0
+    if f".{num}" in s:
+        if not f"{num}.{num}" in s:
+            return 0
+    sign = s.find('+')
+    if sign == -1:
+        sign = s.find('-')
+    if sign != -1:
+        if not sign+1 == len(s):
+            if s[sign+1] == ' ':
+                return 0
+    if int(num) > 2**31 - 1:
+        num = 2**31 - 1
+    elif int(num) < -2**31:
+        num = -2**31
+    for x in s:
+        if x != ' ' and x != '+' and x != '-':
+            try:
+                int(x)
+            except ValueError:
+                return 0
+            else:
+                return int(num)
+    return int(num)
